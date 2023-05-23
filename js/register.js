86d62641d6f7b56b4514eeb3704d2e010af64c5d
KaiKiat <92348692+Kaikiat1126@users.noreply.github.com>
2023-05-23 15:19:59 +0800

    feat:✨valid email exist

--- js/register.js
+++ js/register.js
@@ -10,10 +10,11 @@ function handleToggleIcon(){
     });
 }
 
+const name = $('#input-area input[name="username"]');
+const email = $('#input-area input[name="email"]');
+const password = $('#input-area input[name="password"]');
+
 function handleRegister(){
-    const name = $('#input-area input[name="username"]');
-    const email = $('#input-area input[name="email"]');
-    const password = $('#input-area input[name="password"]');
     const data = {
         name: name.val(),
         email: email.val(),
@@ -23,8 +24,6 @@ function handleRegister(){
 
     if(data.username === '' || data.email === '' || data.password === '') {
         alert("Don't leave any field blank!");
-        // const modal = new Modal('warning','Warning', 'Don\'t leave any field blank');
-        // modal.render();
         return false;
     }
 
@@ -32,16 +31,25 @@ function handleRegister(){
 
     if(!isValid){
         alert("Invalid input!");
-        // const modal = new Modal('error','Error', 'Invalid input');
-        // modal.render();
         return;
     }
 
-    //console.log("data: ", data);
-    
-    const query = `INSERT INTO users (NAME, EMAIL, PASSWORD, ROLE) VALUES ('${data.name}', '${data.email}', '${data.password}', '${data.role}')`
+    const emailQuery = `SELECT COUNT(*) AS count FROM users WHERE EMAIL = '${email}'`;
 
-    //TODO: send data to server
+    $.ajax({
+        url: "../php/authentication.php",
+        type: "GET",
+        data: {query: emailQuery},
+        success: function (data) {
+            if(data.count > 0){
+                alert("Email already exist!");
+                return;
+            }
+        }
+    });
+    
+    const query = `INSERT INTO users (NAME, EMAIL, PASSWORD, ROLE) VALUES ('${data.name}', '${data.email}', '${data.password}', '${data.role}')`;
+
     $.ajax({
         url: "../php/create-user.php",
         type: "POST",

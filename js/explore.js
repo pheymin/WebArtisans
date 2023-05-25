e4a9dd1cb7dbc38e19df4142a4e20e5442dccea6
pheymin <pheymin1223@gmail.com>
2023-05-26 01:34:16 +0800

    feat:✨ add lesson basic info & videoUrl

--- js/explore.js
+++ js/explore.js
@@ -32,7 +32,7 @@ function loadData(data) {
         $.each(data, function (index, item) {
             // Create new card
             let card = new Lesson(item);
-            let cardElement = getCard(card);
+            let cardElement = card.getCard();
 
             cardContainer.append(cardElement);
 
@@ -62,31 +62,11 @@ $('#search-btn').click(function () {
     });
 });
 
-$('#search-bar').on('change', function () {
+$('#search-bar').on('input', function () {
     if ($('#search-bar').val() === "") {
         getAllData();
     }
 });
-
-function getCard(lesson) {
-    let shortenDes = lesson.getShortenDes();
-
-    let template = `
-      <div id="card" class="card card-compact w-full cursor-pointer border border-solid border-gray bg-white hover:bg-white hover:drop-shadow-md">
-          <figure><img src="${lesson.coverPic}" alt="cover_pic" class="object-cover"/></figure>
-          <div class="card-body">
-              <h4 class="card-title text-lg">${lesson.title}</h4>
-              <p>${shortenDes}</p>
-              <div>
-                  <p class="text-[#86909c] text-xs">${lesson.lecturer}</p>
-                  <p class="text-[#86909c] text-xs">Publish on ${lesson.upload_time}</p>
-              </div>
-          </div>
-      </div>
-    `;
-
-    return $(template);
-}
 
 function getNoResult(type) {
     let template = `test`;
--- js/lessons.js
+++ js/lessons.js
@@ -19,4 +19,24 @@ export class Lesson {
 
         return shortenDes;
     }
+
+    getCard() {
+        let shortenDes = this.getShortenDes();
+    
+        let template = `
+          <div id="card" class="card card-compact w-full cursor-pointer border border-solid border-gray bg-white hover:bg-white hover:drop-shadow-md">
+              <figure><img src="${this.coverPic}" alt="cover_pic" class="object-cover"/></figure>
+              <div class="card-body">
+                  <h4 class="card-title text-lg text-ellipsis">${this.title}</h4>
+                  <p text-ellipsis>${shortenDes}</p>
+                  <div>
+                      <p class="text-[#86909c] text-xs text-ellipsis">${this.lecturer}</p>
+                      <p class="text-[#86909c] text-xs">Publish on ${this.upload_time}</p>
+                  </div>
+              </div>
+          </div>
+        `;
+    
+        return $(template);
+    }
 }
